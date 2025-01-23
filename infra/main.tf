21a40f5aa70ce69eb3d9332ab236e556e48e00ff
Yuri Chernushenko <y.chernushenko@gmail.com>
2025-01-23 14:18:51 +0100

    add access rights

--- infra/main.tf
+++ infra/main.tf
@@ -224,6 +224,7 @@ resource "aws_iam_policy" "prefect_work_pool_task" {
           "ecs:DescribeTasks",
           "ecs:StopTask",
           "ecs:RegisterTaskDefinition",
+          "ec2:DescribeVpcs",
           "iam:PassRole"
         ]
         Resource = "*"
