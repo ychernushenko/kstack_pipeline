67e74edf90781e786af5f9fa5ab26e614dd3357c
Yuri Chernushenko <y.chernushenko@gmail.com>
2025-01-23 15:20:16 +0100

    add cluster

--- src/create_deployment.py
+++ src/create_deployment.py
@@ -6,7 +6,9 @@ def create_deployment(name, work_pool_name, image, build, push):
         name,
         work_pool_name=work_pool_name,
         image=image,
-        build=build
+        cluster="arn:aws:ecs:eu-central-1:742491319596:cluster/prefect-cluster",
+        build=build,
+        push=push,
     )
 
 if __name__ == "__main__":
@@ -16,7 +18,8 @@ if __name__ == "__main__":
     parser.add_argument("--work_pool_name", required=True, help="Name of the work pool")
     parser.add_argument("--image", required=False, help="Docker image")
     parser.add_argument("--build", type=bool, default=False, help="Build flag")
+    parser.add_argument("--push", type=bool, default=False, help="Push flag")
     args = parser.parse_args()
 
     # Call create_deployment with parsed arguments
-    create_deployment(args.name, args.work_pool_name, args.image, args.build)
+    create_deployment(args.name, args.work_pool_name, args.image, args.build, args.push)
